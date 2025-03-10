e38cb2ac2996ab1c0bfe5a23df3f4d424b67a1f6
gdm <harukikoga.dev@gmail.com>
2025-03-10 23:05:12 +0900

    プロンプト修正

--- shared/src/commonMain/kotlin/com/example/hanas/domain/util/ChatPromptFactory.kt
+++ shared/src/commonMain/kotlin/com/example/hanas/domain/util/ChatPromptFactory.kt
@@ -15,19 +15,19 @@ object ChatPromptFactory {
                 Now, You must chat about $topic.
 
                 Your response must include the following information:  
-                1. Formatted user's message (e.g., capitalization, quotation marks) and output it as is.
+                1. Formatted user's message (e.g., capitalization, quotation marks, add period/comma).
                 2. Your response (principally 1–2 sentences, within 70 words, in English only).
                 3. A Japanese translation of your response.
                 4. An example of a user's reply in Japanese to your response (principally 1 sentence).
                 
                 Your response must be presented in the following JSON format.
-                All strings enclosed in quotation marks (“”) should be of type string.
+                All strings enclosed in quotation marks ("") should be of type string.
 
                 {
-                  “formatted_prompt”: “1”,
-                  "content": “2”,
-                  “translated_content": “3",
-                  "answer_hint": “4”
+                  "formatted_prompt": "1",
+                  "content": "2",
+                  "translated_content": "3",
+                  "answer_hint": "4"
                 }
                 """.trimIndent(),
         )
